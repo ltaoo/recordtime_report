89dbc7fe148990b8de61ccf90183bbda5e711fed
ltaoo <litaowork@aliyun.com>
2018-08-29 20:09:14 +0800

    feat: 增删改查代码

---
+++ serverless/README.md
@@ -0,0 +1,41 @@
+# 参考资料
+
+## Node
+
+### Node 入口函数
+
+`event`的具体值
+
+```json
+{
+    "body": "",
+    "headers":
+    {
+        "Upgrade-Insecure-Requests": "1",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "X-Ca-Api-Gateway": "619CCFCB-404F-4C89-B590-7A814A7C4A31",
+        "Accept-Encoding": "gzip, deflate",
+        "X-Real-IP": "115.236.92.74",
+        "X-Forwarded-Proto": "http",
+        "X-Forwarded-For": "115.236.92.74",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "CA-Host": "6f6543b4ff354955a3d2ba0475c49999-cn-hangzhou.alicloudapi.com"
+    },
+    "httpMethod": "GET",
+    "isBase64Encoded": true,
+    "path": "/",
+    "pathParameters": {},
+    "queryParameters": {}
+}
+```
+
+https://help.aliyun.com/document_detail/62213.html?spm=a2c4g.11186623.4.4.38a6793bEkofeT#nodejs
+
+### Node 环境
+
+https://help.aliyun.com/document_detail/58011.html?spm=a2c4g.11186623.2.7.518b1471A83Oks
+
+## 错误类型
+
+https://help.aliyun.com/document_detail/52736.html?spm=a2c4g.11186623.2.22.49165ed6BCJDR4
--- serverless/index.js
+++ serverless/index.js
@@ -1,87 +1,97 @@
-'use strict';
-
+/**
+ * @file 增删改查
+ * @author wuya
+ */
 const TableStore = require('tablestore');
 const Long = TableStore.Long;
 
-async function getClient(context) {
-    return new TableStore.Client({
-        accessKeyId: context.credentials.accessKeyId,
-        secretAccessKey: context.credentials.accessKeySecret,
-        stsToken: context.credentials.securityToken,
-        endpoint: process.env['Endpoint'],
-        instancename: process.env['InstanceName']
-    });
+/**
+ * 连接数据库
+ * @param {*} context 
+ * @return {Client} - 数据库实例
+ */
+async function connect(context) {
+  return new TableStore.Client({
+    accessKeyId: context.credentials.accessKeyId,
+    secretAccessKey: context.credentials.accessKeySecret,
+    stsToken: context.credentials.securityToken,
+    endpoint: process.env['Endpoint'],
+    instancename: process.env['InstanceName'],
+  });
 }
 
-async function getCount(client) {
-    var params = {
-        tableName: process.env['TableName'],
-        primaryKey: [{ 'count_name': 'views' }],
-        maxVersions: 1
-    };
+/**
+ * 查询数据
+ * @param {*} client 
+ */
+async function fetch(client) {
+  const params = {
+    tableName: process.env['TableName'],
+    primaryKey: [{ count_name: 'views' }],
+    maxVersions: 1,
+  };
 
-    const tableName = process.env['TableName'];
-    const response = await client.getRow(params);
-    const row = response.row;
+  const response = await client.getRow(params);
+  const row = response.row;
 
-    if (row && row.primaryKey) {
-        return row.attributes[0].columnValue.toNumber();
-    }
-    return null;
+  if (row && row.primaryKey) {
+    return row.attributes[0].columnValue.toNumber();
+  }
+  return null;
 }
 
-exports.handler = function(event, context, callback) {
+/** 
+ * 新增记录
+ */
+async function create(client, options) {
+  const views = 10;
+  return await client.updateRow({
+    tableName: process.env['TableName'],
+    condition: new TableStore.Condition(
+      TableStore.RowExistenceExpectation.EXPECT_NOT_EXIST,
+      null,
+    ),
+    primaryKey: [{ count_name: 'views' }],
+    updateOfAttributeColumns: [
+      { PUT: [{ count: Long.fromNumber(views) }] },
+    ],
+  });
+}
 
-    (async() => {
-        let views = null;
+/**
+ * 更新指定记录
+ * @param {Client} client 
+ * @param {*} options 
+ */
+async function update(client, options) {
+  const views = 1;
+  return await client.updateRow({
+    tableName: process.env['TableName'],
+    condition: new TableStore.Condition(
+      TableStore.RowExistenceExpectation.IGNORE,
+      new TableStore.SingleColumnCondition(
+        'count',
+        Long.fromNumber(views),
+        TableStore.ComparatorType.EQUAL,
+      ),
+    ),
+    primaryKey: [{ count_name: 'views' }],
+    updateOfAttributeColumns: [
+      { PUT: [{ count: Long.fromNumber(views + 1) }] },
+    ],
+    returnContent: { returnType: TableStore.ReturnType.Primarykey },
+  });
+}
 
-        const client = await getClient(context);
-        let success = false;
+exports.handler = async function(event, context, callback) {
+  // 连接数据库
+  // const client = await connect(context);
 
-        do {
-            views = await getCount(client);
+  const response = {
+    isBase64Encoded: false,
+    statusCode: 200,
+    body: event.toString(),
+  };
 
-            if (views) {
-                try {
-                    await client.updateRow({
-                        tableName: process.env['TableName'],
-                        condition: new TableStore.Condition(TableStore.RowExistenceExpectation.IGNORE, new TableStore.SingleColumnCondition('count', Long.fromNumber(views), TableStore.ComparatorType.EQUAL)),
-                        primaryKey: [{ 'count_name': 'views' }],
-                        updateOfAttributeColumns: [
-                            { 'PUT': [{ 'count': Long.fromNumber(views + 1) }] }
-                        ],
-                        returnContent: { returnType: TableStore.ReturnType.Primarykey }
-                    });
-                    success = true;
-                } catch (ex) {
-                    if (ex.code !== 403) {
-                        callback(ex, null);
-                    }
-                }
-            } else {
-                try {
-                    views = 1;
-                    const res = await client.updateRow({
-                        tableName: process.env['TableName'],
-                        condition: new TableStore.Condition(TableStore.RowExistenceExpectation.EXPECT_NOT_EXIST, null),
-                        primaryKey: [{ 'count_name': 'views' }],
-                        updateOfAttributeColumns: [
-                            { 'PUT': [{ 'count': Long.fromNumber(views) }] }
-                        ]
-                    });
-                    success = true;
-                } catch (ex) {
-                    console.log(ex);
-                }
-            }
-        } while (!success);
-
-        var response = {
-            isBase64Encoded: false,
-            statusCode: 200,
-            body: `You are the ${views}th visitor!`
-        };
-
-        callback(null, response);
-    })();
+  callback(null, response);
 };
